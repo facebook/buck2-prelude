facebook/buck2-prelude

commit de7f2c44275d88e1de528f0923e6140c0bfceaee

Generating dependency graph should include boot files (#38)
.hs-boot, .hs, .hsc, all such files should be included in the dependency graph generation.

* Generating dependency graph should include boot files
* handle haskell_boot_sources separately
* boot_apparent_name from boot_sources, not from sources.
this gives correct behavior with generated hs files (from hsc2hs for example)
* -boot module keys should not be added as exposed modules in conf
* undo unnecessary change
* no need for special hs-boot treatment

haskell/tools/generate_target_metadata.py
@@ -142,7 +142,7 @@ def determine_module_mapping(ghc_depends, source_prefix):
             if len(boot_sources) != 1:
                 raise RuntimeError(f"Expected at most one Haskell boot file for module '{modname}' but got '{boot_sources}'.")
 
-            boot_apparent_name = src_to_module_name(strip_prefix_(source_prefix, sources[0]).lstrip("/")) + "-boot"
+            boot_apparent_name = src_to_module_name(strip_prefix_(source_prefix, boot_sources[0]).lstrip("/")) + "-boot"
 
             if boot_apparent_name != boot_modname:
                 result[boot_apparent_name] = boot_modname
@@ -190,6 +190,7 @@ def run_ghc_depends(ghc, ghc_args, sources, aux_paths):
         json_fname = os.path.join(dname, "depends.json")
         make_fname = os.path.join(dname, "depends.make")
         haskell_sources = list(filter(is_haskell_src, sources))
+        haskell_boot_sources = list(filter (is_haskell_boot, sources))
         args = [
             ghc, "-M", "-include-pkg-deps",
             # Note: `-outputdir '.'` removes the prefix of all targets:
@@ -197,7 +198,7 @@ def run_ghc_depends(ghc, ghc_args, sources, aux_paths):
             "-outputdir", ".",
             "-dep-json", json_fname,
             "-dep-makefile", make_fname,
-        ] + ghc_args + haskell_sources
+        ] + ghc_args + haskell_sources + haskell_boot_sources
 
         env = os.environ.copy()
         path = env.get("PATH", "")

commit f66d6e90bd90ff33b14dd7d3dddb87154185e310

Remove boot files from ghc command line
This fixes a regression introduced in commit de7f2c44275d88e1de528f0923e6140c0bfceaee which broke compilation
of sources with cyclic module dependencies.

haskell/tools/generate_target_metadata.py
@@ -190,7 +190,7 @@ def run_ghc_depends(ghc, ghc_args, sources, aux_paths):
         json_fname = os.path.join(dname, "depends.json")
         make_fname = os.path.join(dname, "depends.make")
         haskell_sources = list(filter(is_haskell_src, sources))
-        haskell_boot_sources = list(filter (is_haskell_boot, sources))
+
         args = [
             ghc, "-M", "-include-pkg-deps",
             # Note: `-outputdir '.'` removes the prefix of all targets:
@@ -198,7 +198,7 @@ def run_ghc_depends(ghc, ghc_args, sources, aux_paths):
             "-outputdir", ".",
             "-dep-json", json_fname,
             "-dep-makefile", make_fname,
-        ] + ghc_args + haskell_sources + haskell_boot_sources
+        ] + ghc_args + haskell_sources
 
         env = os.environ.copy()
         path = env.get("PATH", "")